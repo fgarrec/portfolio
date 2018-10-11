5fef29355ecdabdcad140a7b1ec41759b8de74d4
fabrice <fgarrec@hotmail.com>
2018-10-11 11:25:56 -0400

    modif css et jquery

--- public/js/hover.js
+++ public/js/hover.js
@@ -1,45 +1,43 @@
 //for mouse over a project
-var onP = false;
-$(".project-items-img").mouseenter( function(){
-    $(this).addClass("is-translating");
-    $(this).siblings().addClass("is-translating"); 
-    onP = false;
-    console.log(onP);
-});
-$(".project-items-title").mouseenter( function(){
-    $(this).addClass("is-translating");
-    $(this).siblings(".project-items-img").siblings(".project-items-category").addClass("is-translating");
-    onP = true;
-    console.log(onP);
-});
-$(".project-items-category").mouseenter( function(){
-    $(this).addClass("is-translating");
-    $(this).siblings(".project-items-img").siblings(".project-items-title").addClass("is-translating");
-    onP = true;
-    console.log(onP);
-});
-// $(".project-items-category, .project-items-title").mouseout( function(){
-//     onP = false;
-//     console.log(onP);
-// });
-$(".project-items-img").mouseout( function(){
+var $window = $(window);
 
-    $(".project-items-category, .project-items-title").mouseenter( function(){
-        onP = true;
-        console.log("onP on the mouseout = " + onP);
-    });
-
-    
-    if(onP == true){
-        // $(this).css("filter", "brightness(50%)");
-        // $(this).removeClass("is-translating");
-        // $(this).siblings().removeClass("is-translating");
-        console.log("on P, "+onP);
-    }else if(onP == false){
-        $(this).removeClass("is-translating");
-        $(this).siblings().removeClass("is-translating");
-        // $(this).addClass("is-translating");
-        console.log("not on P, "+onP);
-        onP = true
+function checkWidth() {
+    var windowsize = $window.width();
+    // console.log(windowsize);
+    if (windowsize > 575) {
+        
+        $(".project-items-img").mouseenter( function(){
+            $(this).addClass("is-translating");
+            $(this).siblings().addClass("is-translating"); 
+            $(this).css("filter", "brightness(50%)");
+            
+        });
+        $(".project-items-title").mouseenter( function(){
+            $(this).addClass("is-translating");
+            $(this).siblings(".project-items-img").siblings(".project-items-category").addClass("is-translating");
+            $(this).siblings(".project-items-img").css("filter", "brightness(50%)");
+            
+            
+        });
+        $(".project-items-category").mouseenter( function(){
+            $(this).addClass("is-translating");
+            $(this).siblings(".project-items-img").siblings(".project-items-title").addClass("is-translating");
+            $(this).siblings(".project-items-img").css("filter", "brightness(50%)");
+            
+            
+        });
+        
+        
+        $(".project-items-img").mouseout( function(){
+            
+            $(this).removeClass("is-translating");
+            $(this).siblings().removeClass("is-translating");
+            $(this).css("filter", "brightness(100%)");
+            
+        });
     }
-});
+}
+
+checkWidth();
+
+$(window).resize(checkWidth);

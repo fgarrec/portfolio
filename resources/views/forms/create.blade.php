2e6122db34ffea6f12d11fdcc6ac34d298b70243
fabrice <fgarrec@hotmail.com>
2018-10-14 02:52:26 -0400

    images

--- resources/views/forms/create.blade.php
+++ resources/views/forms/create.blade.php
@@ -19,15 +19,15 @@
         {!! Form::label('technos', 'Technos') !!}
         {!! Form::text('technos', null) !!}
     </div>
-    {{--  <div class="form-elements">
+    <div class="form-elements">
         {!! Form::label('vignette', 'Vignette') !!}
         {!! Form::file('vignette') !!}
     </div>
     <div class="form-elements">
         {!! Form::label('img1', 'Image1') !!}
         {!! Form::file('img1') !!}
-    </div>  --}}
-    {{--  <div class="form-elements">
+    </div>  
+     {{--  <div class="form-elements">
         {!! Form::label('img1', 'Image1') !!}
         {!! Form::file('img1') !!}
     </div>  --}}

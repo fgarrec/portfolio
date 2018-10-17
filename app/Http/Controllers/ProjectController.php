d94909a2a02790558c4aa1ed3435b436b333f486
fabrice <fgarrec@hotmail.com>
2018-10-17 13:00:04 -0400

    public path

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -42,12 +42,12 @@ class ProjectController extends Controller
         if($request->hasFile('vignette') && $request->hasFile('img1')){
             $vignette = $request->file('vignette');
             $vignName = 'vignette' . '_' . time() . '.' . $vignette->getClientOriginalExtension();
-            $locationV = url().'/img/vignettes/' . $vignName;
+            $locationV = url('/').'/img/vignettes/' . $vignName;
             Image::make($vignette)->fit(695,460)->save($locationV);
             
             $img1 = $request->file('img1');
             $img1Name = 'img1' . '_' . time() . '.' . $img1->getClientOriginalExtension();
-            $locationI = public_path().'/img/' . $img1Name;
+            $locationI = url('/').'/img/' . $img1Name;
             Image::make($img1)->fit(695,460)->save($locationI);
 
             $project->vignettes = $vignName;

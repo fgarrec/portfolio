bebda6191e6f2ee2e7c8660e87bdbf7640790df9
fabrice <fgarrec@hotmail.com>
2018-10-18 13:50:13 -0400

    optimisation

--- public/js/parallax.js
+++ public/js/parallax.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $('.parallax-header').parallax({imageSrc: 'img/header-portfolio.png'});
+    $('.parallax-header').parallax({imageSrc: 'img/header-portfolio.jpg'});
     $('.parallax-carousel').parallax({imageSrc: 'img/adam-birkett-216660-unsplash.jpg'});
     $('.parallax-skills').parallax({imageSrc: 'img/christopher-gower-291246-unsplash.jpg'});
     $('.parallax-projects').parallax({imageSrc: 'img/zelda.jpg'});
